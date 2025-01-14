e049f3dfe1a78396da46ba81e23b81c6d6e2264e
hector <hectorv117@gmail.com>
2025-01-13 23:15:54 -0600

    added home, end, and delete key functionality

--- c-projects/textEditor/kilo.c
+++ c-projects/textEditor/kilo.c
@@ -18,6 +18,9 @@ enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -96,10 +99,20 @@ int editorReadKey()
                 {
                     switch (seq[1])
                     {
+                    case '1':
+                        return HOME_KEY;
+                    case '3':
+                        return DEL_KEY;
+                    case '4':
+                        return END_KEY;
                     case '5':
                         return PAGE_UP;
                     case '6':
                         return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
                     }
                 }
             }
@@ -116,7 +129,21 @@ int editorReadKey()
                     return ARROW_RIGHT;
                 case 'D':
                     return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
                 }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
             }
         }
 
@@ -292,14 +319,17 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
 
-    case ARROW_UP:
-    case ARROW_DOWN:
-    case ARROW_LEFT:
-    case ARROW_RIGHT:
-        editorMoveCursor(c);
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        printf("hereee");
+        E.cx = E.screencols - 1;
         break;
     case PAGE_UP:
     case PAGE_DOWN:
@@ -309,6 +339,13 @@ void editorProcessKeypress()
             editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
     }
     break;
+    
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
+        break;
     }
 }
 
